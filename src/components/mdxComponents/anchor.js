d4a858093a1341e64f90d8908ba3dc95541e1620
Stefano Pezzino <spezzino.13@gmail.com>
2019-10-31 09:55:11 -0300

    Adds rel=noopener for anchor tags with _blank

--- src/components/mdxComponents/anchor.js
+++ src/components/mdxComponents/anchor.js
@@ -3,7 +3,7 @@ import * as React from "react";
 const AnchorTag = ({ children: link, ...props }) => {
   if(link) {
     return (
-      <a href={props.href} target="_blank">{link}</a>
+      <a href={props.href} target="_blank" rel="noopener">{link}</a>
     );
   } else {
     return null;
