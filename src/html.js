38ae8f8f5ddd9e56f305066cf7935d16a7fabaf6
lastnamearya <arya.jigyasu6815@gmail.com>
2020-03-13 22:03:44 +0530

    Add title tag for a11y

--- src/html.js
+++ src/html.js
@@ -10,6 +10,7 @@ export default class HTML extends React.Component {
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <title>Gatsby Gitbook Boilerplate</title>
           {config.siteMetadata.ogImage ? (
             <meta property="og:image" content={config.siteMetadata.ogImage} />
           ) : null}
