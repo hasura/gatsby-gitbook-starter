b535cd5cbf7e6b453094ed2e10e15004a3fc88a7
Jeong-sik <yousleepwhen@gmail.com>
2021-05-25 13:59:19 +0900

    fix: fix backtick problem

--- src/components/mdxComponents/index.js
+++ src/components/mdxComponents/index.js
@@ -9,24 +9,40 @@ const StyledPre = styled('pre')`
   background: ${props => props.theme.colors.preFormattedText};
 `;
 
+const appendString = (children) => {
+  if(Array.isArray(children)) {
+    return children.reduce((acc, current) => {
+      if(typeof current === "string") {
+        return acc.concat(current)
+      } else if (typeof current === "object") {
+        return acc.concat(current.props.children)
+      } else {
+        return acc
+      }
+    }, "")
+  } else {
+    return children
+  }
+}
+
 export default {
   h1: props => (
-    <h1 className="heading1" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
+    <h1 className="heading1" id={appendString(props.children).replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
   h2: props => (
-    <h2 className="heading2" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
+    <h2 className="heading2" id={appendString(props.children).replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
   h3: props => (
-    <h3 className="heading3" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
+    <h3 className="heading3" id={appendString(props.children).replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
   h4: props => (
-    <h4 className="heading4" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
+    <h4 className="heading4" id={appendString(props.children).replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
   h5: props => (
-    <h5 className="heading5" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
+    <h5 className="heading5" id={appendString(props.children).replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
   h6: props => (
-    <h6 className="heading6" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
+    <h6 className="heading6" id={appendString(props.children).replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
   p: props => <p className="paragraph" {...props} />,
   pre: props => (
