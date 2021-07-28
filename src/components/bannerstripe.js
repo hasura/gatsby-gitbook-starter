0bc10b6239dcf1919175feeaa2b6693a7a33fbfb
surendran82 <surendran@hasura.io>
2021-07-28 10:46:49 +0530

    update top banner (#117)

--- src/components/bannerstripe.js
+++ src/components/bannerstripe.js
@@ -1,19 +1,23 @@
 import React, { Fragment, useState } from "react";
 import "./bannerstripe.css";
 import closeIcon from "./images/close-banner.svg";
+import arrowBlock from "./images/arrow-block.svg";
 const BannerStripe = () => {
   const [showBanner, setShowBanner] = useState(false);
   return (
     <Fragment>
       {!showBanner ? (
-        <div className="hasuraConBannerBg">
+        <div className="commonBannerStripWrapperBg">
           <div className="containerWidth">
             <div className="commonBannerStripWrapper">
               <a
-                href="https://hasura.io/events/hasura-con-2021/?pg=learn-course&plcmt=banner&cta=register-now&tech="
+                href="https://hasura.io/community/community-call/?pg=learn-course&plcmt=header&cta=register&tech="
                 className="articleDesc"
               >
-                HasuraCon'21, our annual user conference, is happening on June 23rd & 24th. Register Now
+                New features being launched at our Community Call. Register to attend
+                <div className='arrow'>
+                  <img src={arrowBlock} alt='Arrow' />
+                </div>
               </a>
               {/*eslint-disable-next-line*/}
               <div
