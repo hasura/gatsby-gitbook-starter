670227f3c46bc588a63cc1e8b8ae5579ab573e27
surendran82 <surendran@hasura.io>
2021-09-28 18:20:53 +0530

    header: remove banner stripe (#132)

--- src/components/themeProvider.js
+++ src/components/themeProvider.js
@@ -4,13 +4,13 @@ import { default as defaultTheme } from './theme';
 import SubHeader from './SubHeader';
 import Header from './Header';
 import Footer from './footer'
-import BannerStripe from "./bannerstripe";
+// import BannerStripe from "./bannerstripe";
 import './styles.css';
 
 export default function ThemeProvider({ children, theme = {}, location }) {
   return (
     <div id="viewport" className="viewport">
-      <BannerStripe location={location} />
+      {/* <BannerStripe location={location} /> */}
       <Header location={location} />
       <SubHeader location={location} />
       <EmotionThemeProvider theme={{ ...defaultTheme, ...theme }}>{children}</EmotionThemeProvider>
