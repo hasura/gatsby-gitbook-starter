84d97bad758062ed7193f82cc93787d030eb1f38
Sai Krishna Prasad Kandula <kskprasad96@gmail.com>
2021-10-12 23:19:46 +0530

    update comment

--- src/utils/customIdParser.js
+++ src/utils/customIdParser.js
@@ -1,5 +1,5 @@
 // Parses the custom added it to keep the url hash un-affected irrespective of translation
-// This is a hot-fix that relies on syntax
+// This is a hot-fix that relies on syntax and works only for headers (# h1 to ###### h6)
 // Ex: ## GraphQL Mutations Title can be in any language {#graphql-mutations}
 // The custom id should be enclosed with in `{#id-in-english}` in english.
 export const customIdParser = (content) => {
