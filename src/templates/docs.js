c522f09b6b4e9f5ffe8dfa2ce77d2ce949ae1e11
Sai Krishna Prasad Kandula <kskprasad96@gmail.com>
2021-10-25 22:10:52 +0530

    handle page helpful response query and track with segment

--- src/templates/docs.js
+++ src/templates/docs.js
@@ -13,6 +13,7 @@ import '../components/styles.css';
 import config from '../../config';
 import gitHub from '../components/images/github.svg';
 import footerIllustration from '../components/images/footer-img.png';
+import { saTrack } from '../utils/segmentAnalytics';
 
 const forcedNavOrder = config.sidebar.forcedNavOrder;
 
@@ -137,8 +138,16 @@ export default class MDXRuntimeTest extends Component {
     super(props);
     this.state = {isSatisfied: null};
   }
+
+  handlePageHelpfulResponse = (response) => {
+    this.setState({
+      isSatisfied: response
+    }, () => saTrack("Responded to Did You Find This Page Helpful", { response: response ? 'YES' : 'NO', page: this.props.location.href })
+  )}
+
   render() {
     const { data, location } = this.props;
+    const { isSatisfied } = this.state;
 
     if (!data) {
       return this.props.children;
@@ -232,10 +241,8 @@ export default class MDXRuntimeTest extends Component {
           <div className="helpfulWrapper">
             <div className="desc">Did you find this page helpful?</div>
             <div className="iconWrapper">
-              <div className={"satisfied" + ((this.state.isSatisfied) ? " active" : "")}
-                onClick={()=> {
-                  this.setState({isSatisfied: true});
-                }}>
+              <div className={"satisfied" + ((isSatisfied) ? " active" : "")}
+                onClick={()=> this.handlePageHelpfulResponse(true)}>
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M15.5 11C16.3284 11 17 10.3284 17 9.5C17 8.67157 16.3284 8 15.5 8C14.6716 8 14 8.67157 14 9.5C14 10.3284 14.6716 11 15.5 11Z"/>
                   <path d="M8.5 11C9.32843 11 10 10.3284 10 9.5C10 8.67157 9.32843 8 8.5 8C7.67157 8 7 8.67157 7 9.5C7 10.3284 7.67157 11 8.5 11Z"/>
@@ -244,17 +251,22 @@ export default class MDXRuntimeTest extends Component {
                   <path d="M11.99 2C6.47 2 2 6.48 2 12C2 17.52 6.47 22 11.99 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 11.99 2ZM12 20C7.58 20 4 16.42 4 12C4 7.58 7.58 4 12 4C16.42 4 20 7.58 20 12C20 16.42 16.42 20 12 20ZM12 17.5C14.33 17.5 16.32 16.05 17.12 14H15.45C14.76 15.19 13.48 16 12 16C10.52 16 9.25 15.19 8.55 14H6.88C7.68 16.05 9.67 17.5 12 17.5Z"/>
                 </svg>
                 {
-                  this.state.isSatisfied ? (
+                  isSatisfied ? (
                     <div className="toolTip">Thanks for your feedback</div>
                   ) : null
                 }
               </div>
-              <div className={"dissatisfied" + ((this.state.isSatisfied === false) ? " active" : "")} onClick={()=>this.setState({isSatisfied: false})}>
+              <div className={"dissatisfied" + ((isSatisfied === false) ? " active" : "")} onClick={()=> this.handlePageHelpfulResponse(false)}>
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M15.5 11C16.3284 11 17 10.3284 17 9.5C17 8.67157 16.3284 8 15.5 8C14.6716 8 14 8.67157 14 9.5C14 10.3284 14.6716 11 15.5 11Z"/>
                   <path d="M8.5 11C9.32843 11 10 10.3284 10 9.5C10 8.67157 9.32843 8 8.5 8C7.67157 8 7 8.67157 7 9.5C7 10.3284 7.67157 11 8.5 11Z"/>
                   <path d="M11.99 2C6.47 2 2 6.48 2 12C2 17.52 6.47 22 11.99 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 11.99 2ZM12 20C7.58 20 4 16.42 4 12C4 7.58 7.58 4 12 4C16.42 4 20 7.58 20 12C20 16.42 16.42 20 12 20ZM12 14C9.67 14 7.68 15.45 6.88 17.5H8.55C9.24 16.31 10.52 15.5 12 15.5C13.48 15.5 14.75 16.31 15.45 17.5H17.12C16.32 15.45 14.33 14 12 14Z"/>
                 </svg>
+                {
+                  isSatisfied !== null && !isSatisfied ? (
+                    <div className="toolTip">Thanks for your feedback</div>
+                  ) : null
+                }
               </div>
             </div>
           </div>
