bcd3fde501f3e2032cef9ef2ee434ef241eab419
Terri Ann <terri.ann.s@gmail.com>
2021-11-20 18:07:30 -0500

    Add support for relative links in markdown files and include examples.
    Addresses hasura/gatsby-gitbook-starter/#104

--- src/components/mdxComponents/anchor.js
+++ src/components/mdxComponents/anchor.js
@@ -1,12 +1,22 @@
 import * as React from 'react';
+import { Link as GatsbyLink } from 'gatsby';
+import isAbsoluteUrl from 'is-absolute-url';
 
 const AnchorTag = ({ children: link, ...props }) => {
   if (link) {
-    return (
-      <a href={props.href} target="_blank" rel="noopener noreferrer">
-        {link}
-      </a>
-    );
+    if (isAbsoluteUrl(props.href)) {
+      return (
+        <a href={props.href} target="_blank" rel="noopener noreferrer">
+          {link}
+        </a>
+      );
+    } else {
+      return (
+        <GatsbyLink to={props.href} {...props}>
+          {link}
+        </GatsbyLink>
+      );
+    }
   } else {
     return null;
   }
