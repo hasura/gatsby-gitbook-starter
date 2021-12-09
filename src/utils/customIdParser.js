c31295dce0caddaae0ed5c6d31322d13d40d08e8
Praveen Durairaju <praveen@hasura.io>
2021-12-09 12:52:00 +0530

    update parser to handle undefined content (#154)

--- src/utils/customIdParser.js
+++ src/utils/customIdParser.js
@@ -3,16 +3,22 @@
 // Ex: ## GraphQL Mutations Title can be in any language {#graphql-mutations}
 // The custom id should be enclosed with in `{#id-in-english}` in english.
 export const customIdParser = (content) => {
-  const [text, id] = content.split("{#");
-  if (!!text && !!id) {
-    return {
-      content: text,
-      id: id.replace('}', '').replace(/\s+/g, '').toLowerCase()
+  if(content !== undefined) {
+    const [text, id] = content.split("{#");
+    if (!!text && !!id) {
+      return {
+        content: text,
+        id: id.replace('}', '').replace(/\s+/g, '').toLowerCase()
+      }
     }
-  }
-
-  return {
-    content,
-    id: content.replace(/\s+/g, '').toLowerCase()
+    return {
+      content,
+      id: content.replace(/\s+/g, '').toLowerCase()
+    }
+  } else {
+    return {
+      content,
+      id: '#' 
+    }
   }
 }
