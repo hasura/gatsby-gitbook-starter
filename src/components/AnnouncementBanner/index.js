6303ed5389a955bc604f53a34362dc18d36c3265
surendran <surendran@hasura.io>
2022-11-16 11:30:01 +0530

    egc airmeet url change to enterprisegraphql

--- src/components/AnnouncementBanner/index.js
+++ src/components/AnnouncementBanner/index.js
@@ -54,11 +54,11 @@ export const AnnouncementBanner = ({ hideThinBanner }) => {
     return (
       <>
         <div className="thinBannerWrapper">
-          <a href="https://www.airmeet.com/e/8e055e00-2f7c-11ed-a8d8-87ae1ec30e05?preview=true" className="flex-center" target="_blank" rel="noopener noreferrer">
+          <a href="https://hasura.io/enterprisegraphql/" className="flex-center">
             <div className="flex-center">
               {/* <div className="greenCircle" /> */}
               <div className="textGradient" fontWeight="font_bold">
-                <div className="greenCircle pinkCircle" />
+                {/* <div className="greenCircle pinkCircle" /> */}
                 <span className="displayInline">
                   Enterprise GraphQL conference is over. View recordings
                 </span>
